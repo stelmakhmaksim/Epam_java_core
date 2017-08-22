569aec32a6eb134fb1b089a5fda424bf2739566a
stelmakhmaksim <stelmakh96@mail.ru>
2017-08-22 21:49:00 +0300

    Deleted main!

---
+++ src/task1/calc/Addition.java
@@ -0,0 +1,19 @@
+package task1.ar;
+/*
+Делаем четыре класса, каждый из которых отвечает за одну математическую операцию - сложение, вычитание, умножение, деление.
+Каждый класс имеет метод public static void main, принимает два параметра из командной строк, возвращает результат.
+Плюсом будет проверка, что количество параметров соответствует ожидаемому, параметры ожидаемого типа.
+ */
+public class Addition {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Wrong number of parameters!");
+        } else {
+            try {
+                System.out.println(Double.parseDouble(args[0]) + Double.parseDouble(args[1]));
+            } catch (NumberFormatException e) {
+                System.out.println("Parameters of unexpected type! " + e);
+            }
+        }
+    }
+}
---
+++ src/task1/calc/Subtraction.java
@@ -0,0 +1,13 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Wrong number of parameters!");
+        } else {
+            try {
+                System.out.println(Double.parseDouble(args[0]) - Double.parseDouble(args[1]));
+            } catch (NumberFormatException e) {
+                System.out.println("Parameters of unexpected type! " + e);
+            }
+        }
+    }
+}
